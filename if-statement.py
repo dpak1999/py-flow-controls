a727ddd56cb1238171d430f8d0ff38d82d07edf3
deepak kumar dash <deepakkumardash313@gmail.com>
2019-12-23 17:44:53 +0530

    initial commit

---
+++ if-statement.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 23 16:50:14 2019
+
+@author: DEEPAK KUMAR DASH
+"""
+#FLOW CONTROL
+a = True
+if a == True:
+    print("Welcome")
+    print("To")
+    print("GIET")
+if a:
+    print('University')
+    print('Gunupur')
+if a:
+    print('you guys are awsome')
+    print('carry on')
+#-----------------------------------------
+num = 100
+if(num>200):
+    print('i got printed')
+print('no matter what i will always be printed')
+#------------------------------------------------------
+str1=input('enter your name')
+if(str1):
+    print('you are a great guy '+str1)
+#--------------------------------------
